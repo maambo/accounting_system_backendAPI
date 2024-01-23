d46c4e87b5f10d29b13823c77f04ff9cb83fd828
maambo <maambomunkuli21@gmail.com>
2024-01-24 01:22:10 +0200

    dashboad data

---
+++ users/dashboad_data.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:51383");
+include '../config.php';
+//if (!empty($_POST['userid'])) {
+    $userId = "1";//$_POST['userid'];
+
+    $fqry = $conn->query("SELECT * FROM users where id='$userId'");
+
+    $data = array();
+    if ($fqry->num_rows > 0) {
+        $userDetails = $fqry->fetch_assoc();
+        $company_id = $userDetails["Company_id"];
+
+        $fqry1 = $conn->query("SELECT * FROM company_details where id='$company_id'");
+        $companyDetails = $fqry1->fetch_assoc();
+
+        $data1 = array_merge(['status' => 200], $userDetails);
+        $data[] = array_merge($data1, $companyDetails);
+
+        echo json_encode($data);
+    } else {
+        echo json_encode("202");
+    }
+
+//} else {
+//    echo json_encode("404");
+//}
