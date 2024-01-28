6d2d828b34a860ccb871ffb753e657dac28c89b3
maambo <maambomunkuli21@gmail.com>
2024-01-28 23:15:32 +0200

    login

--- users/login.php
+++ users/login.php
@@ -1,22 +1,25 @@
 <?php 
 header("Access-Control-Allow-Origin: http://localhost:51383");
 	include '../config.php';
-	if(!empty($_POST['username']) && !empty($_POST['password'])){
-	$username = $_POST['username'];
-    $password = $_POST['password'];
-	$fqry = $conn->query("SELECT id FROM users where username='$username' and password = '$password' ");
+
 	$data = array();
+
+	if($_GET['username'] && $_GET['password']){
+	$username = $_GET['username'];
+    $password = $_GET['password'];
+	$fqry = $conn->query("SELECT id FROM users where username='$username' and password = '$password' ");
+	
 	if($fqry->num_rows > 0){
-while($row = $fqry->fetch_assoc()){
-	$data[]= array_merge($row,['status'=>200]);
-}
+    $row = $fqry->fetch_assoc();
+	$data[]=array_merge(array('status'=>200), $row);
+
+
 		echo json_encode($data);
 }else{
-    echo json_encode("202");
+	$data[]=array('status'=>202);
+    echo json_encode($data);
 }
 
-}else{
-		echo json_encode("404");
 }
 
 
