4bd2bb8bc63e7b637ac43d437c55c64370784a32
maambo <maambomunkuli21@gmail.com>
2024-04-22 00:23:20 +0200

    registration user backend

--- users/Registration_user.php
+++ users/Registration_user.php
@@ -1,56 +1,54 @@
-<?php 
+<?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-	include '../config.php';
-	if(isset($_POST['username'])){
-	$username = $_POST['username'];
+include '../config.php';
+
+if(isset($_POST['username'])) {
+    $username = $_POST['username'];
     $password = $_POST['password'];
-	$phone_no = $_POST['phone_no'];
-	$job_tittle =$_POST['job_tittle'];
-	$access = $_POST['access'];
-	$fqry = $conn->query("SELECT * FROM users where username='$username'");
-	
-	if($fqry->num_rows > 0){
+    $phone_no = $_POST['phone_no'];
+    $nrc = $_POST['nrc'];
+    $job_tittle = $_POST['job_tittle'];
+    $access = $_POST['access'];
 
-	echo json_encode("user Exists");
-}else{
+    $fqry = $conn->query("SELECT * FROM users where username='$username'");
 
-	if(isset($_FILES['img'])){// if uploaded
-		$img_name = $_FILES['img']['name']; //geting user uploaded img name
-		$img_type = $_FILES['img']['type']; //geting user uploaded img type
-		$tmp_name = $_FILES['img']['tmp_name'];// temporary name is used to save/move file in our folder
+    if($fqry->num_rows > 0) {
+        echo json_encode("user Exists");
+    } else {
+        if(isset($_POST['img']) && !empty($_POST['img'])) { // If image is posted
+            $base64_image = $_POST['img'];
+            $img_data = base64_decode($base64_image);
 
-		//let,s explode image and get the last extention like jpeg png
-		$img_explode =explode('.', $img_name);
-		$img_ext = end($img_explode);//get extention
+            $time = time(); // Unique filename
+            $new_img_name = $time . ".png"; // Assume PNG format, you can handle other formats as well
 
-		$extentions =['png','jpeg','jpg','PNG','JPEG','JPEG']; //valid extentions
-		if(in_array($img_ext, $extentions) === true){///check the image extention
-			$time = time(); //this will return the current time so the that an image has a unique name
-			
-			
-			$new_img_name = $time.$img_name;
-		   
-			if(move_uploaded_file($tmp_name, "./user_img/".$new_img_name)){//if sucessfully moved all mved image to our folder
-				if(empty($id)){
+            if(file_put_contents("./user_img/" . $new_img_name, $img_data)) {
+                // If image saved successfully
+                if(empty($id)) {
+                    // If no id is provided, insert a new record
+                    $fqry1 = $conn->query("INSERT into users(username, password, phone_no, nrc, job_title, image, access) 
+                                           VALUES('".$username."','".$password."','".$phone_no."','".$nrc."','".$job_tittle."','".$new_img_name."','".$access."')");
+                } else {
+                    // If id is provided, update the existing record
+                    $fqry1 = $conn->query("UPDATE users 
+                                           SET username ='{$username}', password = '{$password}', phone_no = '{$phone_no}', 
+                                           TPIN = '{$TPIN}', job_tittle = '{$job_tittle}', image ='{$new_img_name}', 
+                                           access = '{$access}' 
+                                           WHERE id = '{$id}'");
+                }
 
-
-	if(!empty($id)){
-		$fqry1 = $conn->query("INSERT into users(username, password, phone_no, TPIN, job_tittle, image, access) VALUES('".$username."','".$password."','".$phone_no."','".$TPIN."','".$job_tittle."','".$new_img_name."','".$access."'");
-	}else{
-		$fqry1 = $conn->query("UPDATE users set username ='{$username}', password = '{$password}', phone_no = '{$phone_no}', TPIN = '{$TPIN}', job_tittle = '{$job_tittle}', image ='{$new_img_name}', access = '{$access}' where id = '{$id}'");
-	}
-   
-    if($fqry1){
-        echo json_encode("success");
+                if($fqry1) {
+                    echo json_encode("success");
+                } else {
+                    echo json_encode("Failed to update database");
+                }
+            } else {
+                echo json_encode("Failed to save image");
+            }
+        } else {
+            echo json_encode("No image data received");
+        }
     }
-}}
-
-	}else{
-		echo json_encode("500");
-	}
-
-		}else{
-			echo "no image selected";
-		}}}
+}
 ?>
